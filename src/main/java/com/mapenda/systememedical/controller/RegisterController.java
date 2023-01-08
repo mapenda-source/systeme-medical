af7910225778e2200036eeda74d89bd4f5d00837
mapenda-source <mapendadioum3@gmail.com>
2023-01-08 16:19:07 -0500

    creer la methode ShowRegistrationPage

--- src/main/java/com/mapenda/systememedical/controller/RegisterController.java
+++ src/main/java/com/mapenda/systememedical/controller/RegisterController.java
@@ -1,8 +1,23 @@
 package com.mapenda.systememedical.controller;
 
+import com.mapenda.systememedical.entity.User;
+import com.mapenda.systememedical.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegisterController {
+
+
+
+    @RequestMapping(value="/register", method = RequestMethod.GET)
+
+    public ModelAndView showRegistrationPage(ModelAndView modelAndView, User user){
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("register");
+        return modelAndView;
+    }
 
 }
